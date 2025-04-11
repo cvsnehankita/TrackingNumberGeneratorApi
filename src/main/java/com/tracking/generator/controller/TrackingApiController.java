f62fbad3fa945079f924952484785cb599231a4f
cvsnehankita <cvsnehankita@gmail.com>
2025-04-11 16:29:58 +0530

    Validation implemented on requestparamn

--- src/main/java/com/tracking/generator/controller/TrackingApiController.java
+++ src/main/java/com/tracking/generator/controller/TrackingApiController.java
@@ -2,16 +2,23 @@ package com.tracking.generator.controller;
 
 import com.tracking.generator.entity.TrackingRequest;
 import com.tracking.generator.service.TrackingApiService;
+import jakarta.validation.constraints.Max;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping("/next-tracking-number")
+@Validated
 public class TrackingApiController {
 
     @Autowired
@@ -22,6 +29,7 @@ public class TrackingApiController {
             @RequestParam String destination_country_id,
             @RequestParam Integer weight,
             @RequestParam String created_at,
+            @RequestParam String created_by,
             @RequestParam String customer_id,
             @RequestParam String customer_name,
             @RequestParam String customer_slug
@@ -29,10 +37,11 @@ public class TrackingApiController {
         TrackingRequest request = new TrackingRequest();
         request.setOriginCountryId(origin_country_id);
         request.setDestinationCountryId(destination_country_id);
-        request.setWeight(weight);
+        request.setWeight(BigDecimal.valueOf(weight));
         SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
-        request.setCreatedAt(sdf.parse(created_at));
+        request.setCreatedAt(ZonedDateTime.now());
         request.setCustomerId(customer_id);
+        request.setCreatedBy(created_by);
         request.setCustomerName(customer_name);
         request.setCustomerSlug(customer_slug);
         return trackingApiService.getTrackingNumber(request);
--- src/main/java/com/tracking/generator/entity/TrackingRequest.java
+++ src/main/java/com/tracking/generator/entity/TrackingRequest.java
@@ -4,51 +4,52 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
+@Getter
 @Entity
 @Data
-@Getter
 @Setter
 public class TrackingRequest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Getter
-    @Setter
+
+    @Size(min = 2, max = 2, message = "Origin country code must be 2 letters (ISO Alpha-2)")
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Origin country code must be uppercase ISO Alpha-2")
     private String originCountryId;
-    @Getter
-    @Setter
+
+    @Size(min = 2, max = 2, message = "Origin country code must be 2 letters (ISO Alpha-2)")
+    @Pattern(regexp = "^[A-Z]{2}$", message = "Origin country code must be uppercase ISO Alpha-2")
     private String destinationCountryId;
-    @Setter
-    @Getter
-    private Integer weight;
-    @Getter
-    @Setter
-    private Date createdAt;
-    @Setter
-    @Getter
+
+    @DecimalMin(value = "0.001", message = "Please enter the order’s weight in kilograms, up to three decimal places")
+    @Digits(integer = 5, fraction = 3)
+    private BigDecimal weight;
+
+    private ZonedDateTime createdAt;
+
+    @NotNull
     private String createdBy;
-    @Setter
-    @Getter
+
+    @NotNull
     private String customerId;
-    @Setter
-    @Getter
+
+    @NotNull
     private String customerName;
-    @Setter
-    @Getter
+
+    @NotNull
+    @Pattern(regexp = "^[a-z0-9]+(?:-[a-z0-9]+)*$", message = "The customer’s name in slug-case/kebab-case (e.g. redbox-logistics)")
     private String customerSlug;
-    public TrackingRequest(){}
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
+    public TrackingRequest() {
     }
 
     public String getOriginCountryId() {
@@ -67,19 +68,19 @@ public class TrackingRequest {
         this.destinationCountryId = destinationCountryId;
     }
 
-    public Integer getWeight() {
+    public BigDecimal getWeight() {
         return weight;
     }
 
-    public void setWeight(Integer weight) {
+    public void setWeight(BigDecimal weight) {
         this.weight = weight;
     }
 
-    public Date getCreatedAt() {
+    public ZonedDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
+    public void setCreatedAt(ZonedDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
