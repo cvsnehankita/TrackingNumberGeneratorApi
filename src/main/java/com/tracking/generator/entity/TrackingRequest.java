934d8ee90b1137b31c6336ffa8d7c3b433b8be84
cvsnehankita <cvsnehankita@gmail.com>
2025-04-11 16:39:34 +0530

    validation added for valid UUID as customer id

--- src/main/java/com/tracking/generator/entity/TrackingRequest.java
+++ src/main/java/com/tracking/generator/entity/TrackingRequest.java
@@ -40,6 +40,9 @@ public class TrackingRequest {
     private String createdBy;
 
     @NotNull
+    @Pattern(
+            regexp = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$",
+            message = "Please enter a valid UUID")
     private String customerId;
 
     @NotNull
